02b8a1f91e3ebee63df6dd9622cbe0b1fe39ff18
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-27 12:16:50 +0530

    Added : Program for the UC8 to UC10 all condition with all test case

--- src/test/java/com/bridgelabz/UserRegistation_LAMBDA/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserRegistation_LAMBDA/UserValidationTest.java
@@ -6,35 +6,35 @@ import org.junit.Test;
 public class UserValidationTest {
 	@Test
 	public void firstName_Validation() {
-		UserValidationTest validation = new UserValidationTest();
+		UserValidation validation = new UserValidation();
 		boolean actualResult = validation.firstName("Manish");
 		Assert.assertTrue(actualResult);
 	}
 
 	@Test
 	public void lastName_Validation() {
-		UserValidationTest validation = new UserValidationTest();
+		UserValidation validation = new UserValidation();
 		boolean actualResult = validation.lastName("Kumar");
 		Assert.assertTrue(actualResult);
 	}
 
 	@Test
 	public void email_Validation() {
-		UserValidationTest validation = new UserValidation();
+		UserValidation validation = new UserValidation();
 		boolean actualResult = validation.email("mkaubr007@gmail.com");
 		Assert.assertTrue(actualResult);
 	}
 
 	@Test
 	public void phoneNumber_Validation() {
-		UserValidationTest validation = new UserValidationTest();
+		UserValidation validation = new UserValidation();
 		boolean actualResult = validation.phoneNumber("+918002695700");
 		Assert.assertTrue(actualResult);
 	}
 
 	@Test
 	public void password_Validation() {
-		UserValidationTest validation = new UserValidationTest();
+		UserValidation validation = new UserValidation();
 		boolean actualResult = validation.password("qw1rt@Ms");
 		Assert.assertTrue(actualResult);
 	}
